fe0b1d0030ca1ebca12d8435b9514f650ad0ffae
anazlatevasap <ana.zlateva@sap.com>
2015-02-16 11:50:47 +0200

    Initial Commit

---
+++ Tutorial2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tutorial2
+//
+//  Created by Petrov, Georgi on 2/16/15.
+//  Copyright (c) 2015 Petrov, Georgi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
