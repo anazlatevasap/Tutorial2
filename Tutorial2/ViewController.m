dc369edf22309a5c101c89fd3dd110cb869d6916
anazlatevasap <ana.zlateva@sap.com>
2015-02-16 16:12:41 +0200

    edited version

--- Tutorial2/ViewController.m
+++ Tutorial2/ViewController.m
@@ -10,6 +10,12 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) NSString *username;
+@property (nonatomic, strong) NSString *password;
+@property (weak, nonatomic) IBOutlet UITextField *usernameTextField;
+@property (weak, nonatomic) IBOutlet UITextField *passwordTextFIeld;
+@property (weak, nonatomic) IBOutlet UILabel *notificationLabel;
+
 @end
 
 @implementation ViewController
@@ -17,11 +23,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+
+    self.username = @"bob";
+    self.password = @"securepass";
+    
+    NSLog(@"%@", self.username);
+    NSLog(@"%@", self.password);
+    
+    self.passwordTextFIeld.secureTextEntry = YES;
+    
+    
+    
 }
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+- (IBAction)loginWasPressed:(id)sender {
+    
+    if([self.username isEqualToString:[self.usernameTextField text]])
+    {NSLog(@"Succes");
+        [self.notificationLabel setText:@"congratulation you have logged in!"];
+    }
+    else {
+        NSLog(@"FAILURE");
+        [self.notificationLabel setText:@"enter correct values"];
+        
+    }
+}
+
+- (void)touchesBegan:(NSSet *)touches withEvent:(UIEvent *)event
+{
+    [self.view endEditing:YES];
 }
 
 @end
